3db98484864324bf71a630239506bfb22abccbe1
David Welch <david.m.welch@gmail.com>
2012-08-01 15:46:44 -0500

    BUG: Fixed directory array creation, but month value is output with a leading zero, making it octal

--- directory.sh
+++ directory.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-MAIN_DIR=/ipldev/sharedopt
+# For testing:
+MAIN_DIR=$PWD
+# MAIN_DIR=/ipldev/sharedopt
 # Create the parent directory string
 # Format: YYYY_Milestone
-export PROC_ENV=`date +%Y`
-month=`date +%m`
+PROC_ENV=$(date +%Y)
+month=$(( $(date +%-m) - 1 ))
 # Since winter starts in December, not January, this works better than a mod function
 seasons=(WINTER WINTER SPRING SPRING SPRING SUMMER SUMMER SUMMER AUTUMN AUTUMN AUTUMN WINTER)
 PROC_ENV=${PROC_ENV}_${seasons[$month]}
 
+echo "Who is/are the principle investigator(s): "
+read -a PI
+echo "Enter a short description of this directory: "
+read -a DESCRIPTION
+echo "Enter the wiki page for this directory: "
+read -a WIKIPAGE
 mkdir ${MAIN_DIR}
 mkdir ${MAIN_DIR}/${PROC_ENV}
 # There should be a desriptive README file:
 touch ${MAIN_DIR}/${PROC_ENV}/README
-echo "PI:
-Short description:
-Wiki page:" :>${MAIN_DIR}/${PROC_ENV}/README
+echo "PI: $PI
+Short description: $DESCRIPTION
+Wiki page: $WIKIPAGE" >${MAIN_DIR}/${PROC_ENV}/README
 
 # Directories should be named as follows
-dirs=(src etc src/build_instructions)
-# There should always be a "src" directory for common source files
-mkdir -p ${MAIN_DIR}/${PROC_ENV}/src
-
-# There should be an "etc" directory for reading configuraiton files
-mkdir -p ${MAIN_DIR}/${PROC_ENV}/etc
-
-# Some incomplete instructions on how the programs should be built
-mkdir -p ${MAIN_DIR}/${PROC_ENV}/src/build_instructions
-
-# Base directory for all the results files
-mkdir -p ${MAIN_DIR}/${PROC_ENV}/$(uname -s)_$(uname -p)
+dirs=(src etc src/build_instructions "$(uname -s)_$(uname -p)")
+index=0
+while [ "$index" -lt "${#dirs[@]}" ]; do
+    echo "Making directory "${MAIN_DIR}/${PROC_ENV}/${dirs[$index]}"..."
+    mkdir -p ${MAIN_DIR}/${PROC_ENV}/${dirs[$index]}
+    ((index++))
+done
 
 chmod oug+rwX ${MAIN_DIR}/${PROC_ENV}/$(uname -s)_$(uname -p)
 
